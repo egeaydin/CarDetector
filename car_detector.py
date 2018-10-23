5f4a1bb724b61e5a1f3d82934897762be29ae0c5
ege <egeaydin@gmail.com>
2018-10-23 15:47:33 -0700

    video file handling added to CarDetector class

--- car_detector.py
+++ car_detector.py
@@ -1,10 +1,14 @@
 from gluoncv import model_zoo
+from moviepy.editor import VideoFileClip
+
 
 class CarDetector:
     def __init__(self, video_path):
-        print('starting')
+        self.video = VideoFileClip(video_path)
 
+        print('Video found, starting to download the dataset.')
+
+        # Obtain pre-trained model from Gluon Zoo
         self.network = model_zoo.get_model('ssd_512_resnet50_v1_voc', pretrained=True)
         print('pre-trained data obtained')
 
-        print(video_path)
